759a9f2ab143079af41a0019d99975a46388a12c
idan <idanr15@gmail.com>
2019-10-04 09:51:33 +0300

    add tests

--- src/test/java/myClass.java
+++ src/test/java/myClass.java
@@ -13,4 +13,24 @@ public class myClass {
 	  Thread.sleep(3000);
 	  driver.quit();
   }
+  
+  @Test()
+  public void test2() {
+	  System.setProperty("webdriver.chrome.driver","D:\\java\\seleniumDriver\\Chrome\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://google.com");
+	  long id = Thread.currentThread().getId();
+	  System.out.println("test2 id: " + id);
+	  driver.quit();
+  }
+  
+  @Test()
+  public void test3() {
+	  System.setProperty("webdriver.chrome.driver","D:\\java\\seleniumDriver\\Chrome\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://google.com");
+	  long id = Thread.currentThread().getId();
+	  System.out.println("test3 id: " + id);
+	  driver.quit();
+  }
 }
